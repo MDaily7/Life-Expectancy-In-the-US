583e80f96c8edcbf0a88743542d23e183d03adbc
Angie Corrales <95327338+Angiecore@users.noreply.github.com>
2022-05-14 14:02:15 -0700

    Updating ERD

---
+++ Database/QuickDBD-Database Life Expectancy by Ethnicity.sql
@@ -0,0 +1,36 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/SqGLVZ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Health_Measures" (
+    "State" int   NOT NULL,
+    "County" varchar   NOT NULL,
+    "Premature_Deaths" int   NOT NULL,
+    "Premature_Deaths_AIAN" int   NOT NULL,
+    "Premature_Deaths_Asian" int   NOT NULL,
+    "Premature_Deaths_Black" int   NOT NULL,
+    "Premature_Deaths_Hispanic" int   NOT NULL,
+    "Premature_Deaths_White" int   NOT NULL,
+    "%_Poor_Fair_Health" int   NOT NULL,
+    CONSTRAINT "pk_Health_Measures" PRIMARY KEY (
+        "State"
+     )
+);
+
+CREATE TABLE "Additional_Health_Measures" (
+    "State" int   NOT NULL,
+    "County" varchar   NOT NULL,
+    "Life_Expentancy" int   NOT NULL,
+    "Life_Expentancy_AIAN" int   NOT NULL,
+    "Life_Expentancy_Black" int   NOT NULL,
+    "Life_Expentancy_Hispanic" int   NOT NULL,
+    "Life_Expentancy_White" int   NOT NULL,
+    CONSTRAINT "pk_Additional_Health_Measures" PRIMARY KEY (
+        "County"
+     )
+);
+
+ALTER TABLE "Health_Measures" ADD CONSTRAINT "fk_Health_Measures_State" FOREIGN KEY("State")
+REFERENCES "Additional_Health_Measures" ("County");
+
